0625698b9e79ad0edd5fa273027eabcca324b0a1
dkempen <daan.vankempen2@gmail.com>
2018-02-06 09:45:39 +0100

    Added field, constructors, getters, setters for Artist and performance

--- Agenda/Performance.java
+++ Agenda/Performance.java
@@ -1,4 +1,49 @@
 package Agenda;
 
+import java.time.LocalTime;
+
 public class Performance {
+    private Artist artist;
+    private Stage stage;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public Performance(Artist artist, Stage stage, LocalTime startTime, LocalTime endTime) {
+        this.artist = artist;
+        this.stage = stage;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+    public void setArtist(Artist artist) {
+        this.artist = artist;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
 }
