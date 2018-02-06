269c0d1cdb71319426b39fde9d587ea0aeb555e7
Carlos C <cccadel@avans.nl>
2018-02-06 10:12:50 +0100

    Auto stash before merge of "developer" and "origin/developer"
    created the Schedule & Stage class

--- Agenda/Schedule.java
+++ Agenda/Schedule.java
@@ -1,4 +1,52 @@
 package Agenda;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Schedule {
+    private LocalDate date;
+    private ArrayList<Artist> artists;
+    private ArrayList<Stage> stages;
+    private ArrayList<Performance> performances;
+    private String name;
+
+    public Schedule(int day, int month, int year, String name) {
+        date = LocalDate.of(year,month,day);
+        this.name = name;
+        artists = new ArrayList<>();
+        stages = new ArrayList<>();
+        performances = new ArrayList<>();
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public ArrayList<Artist> getArtists() {
+        return artists;
+    }
+
+    public ArrayList<Stage> getStages() {
+        return stages;
+    }
+
+    public ArrayList<Performance> getPerformances() {
+        return performances;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addArtist(Artist artist){
+        artists.add(artist);
+    }
+
+    public void addStage(Stage stage){
+        stages.add(stage);
+    }
+
+    public void addPerformace(Performance performance){
+        performances.add(performance);
+    }
 }
--- Agenda/Stage.java
+++ Agenda/Stage.java
@@ -1,4 +1,27 @@
 package Agenda;
 
 public class Stage {
+    String name;
+    int capacity;
+
+    public Stage(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapicity() {
+        return capacity;
+    }
+
+    public void setCapicity(int capicity) {
+        this.capacity = capicity;
+    }
 }
