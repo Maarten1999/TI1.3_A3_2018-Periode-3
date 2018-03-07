5c07121617582e6d71fb16b60348345e8015ae99
Carlos C <cccadel@avans.nl>
2018-03-07 14:51:42 +0100

    test push *ignore*

--- src/agenda/gui/PopupWindow.java
+++ src/agenda/gui/PopupWindow.java
@@ -7,7 +7,7 @@ import agenda.data.Stage;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.event.WindowEvent;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.*;
